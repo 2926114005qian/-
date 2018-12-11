fd309bd5ec585cff156c53cb762503cb4d53a911
2926114005qian <31874249+2926114005qian@users.noreply.github.com>
2018-12-11 10:37:49 +0800

    Create StringMethodDeo.java

---
+++ String类/StringMethodDeo.java
@@ -0,0 +1,22 @@
+package String.Demo;
+
+public class StringMethodDeo {
+
+	public static void main(String[] args) {
+		String str="asgjvcsga";
+		System.out.println(str.length());
+		System.out.println(str.charAt(2));
+//		System.out.println(str.indexOf("s"));
+//		System.out.println(str.indexOf("a",2));
+//		System.out.println(str.indexOf("sgj"));
+//		System.out.println(str.indexOf("sg",2));
+//		System.out.println(str.lastIndexOf("s"));
+//		System.out.println(str.lastIndexOf("a",2));
+//		System.out.println(str.lastIndexOf("sgj"));
+//		System.out.println(str.lastIndexOf("sg",2));
+		System.out.println(str.substring(0, 2));
+		System.out.println(str.toUpperCase());
+		System.out.println(str.toLowerCase());
+	}
+
+}
