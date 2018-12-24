91a14dc01fd574d34f1bade2ab733146b9a28108
2926114005qian <31874249+2926114005qian@users.noreply.github.com>
2018-12-24 14:46:02 +0800

    Create Student.java

---
+++ Map/Student.java
@@ -0,0 +1,63 @@
+package Map;
+
+import Collection.Person;
+
+public class Student implements Comparable{
+	private String name;
+	private int age;
+	public String getName() {
+		return name;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (age != other.age)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	public Student(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	public Student() {
+		super();
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public int compareTo(Object o) {
+		Student p=(Student)o;
+		int temp=this.name.compareTo(p.name);
+		return temp==0?this.age-p.age:temp;
+//		int temp=this.age-p.age;
+//		return temp==0?this.name.compareTo(p.name):temp;
+
+	}
+}
